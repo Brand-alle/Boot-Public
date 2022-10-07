a0237ab7ff5fcd32292fcbd2678f0ed8a485f2f4
Brand-alle <111024959+Brand-alle@users.noreply.github.com>
2022-10-06 20:21:12 -0700

    Add files via upload

---
+++ chaining_methods.py
@@ -0,0 +1,44 @@
+
+
+class user:
+    def __init__(self, first_name, last_name, email, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.age = age
+        self.is_rewards_member = False
+        self.gold_card_points = 0
+
+
+    def display_info(self):
+            print(self.first_name, self.last_name, self.email, self.age, self.is_rewards_member)
+            return self
+
+    def enroll(self):
+        self.gold_card_points = 200
+        if self.is_rewards_member == False:
+                self.is_rewards_member = True
+        else:
+            self.is_rewards_member == False
+            print("User already a member")
+        print(self.is_rewards_member, self.gold_card_points)
+        return self
+
+    def spend_points(self, amount):
+        self.gold_card_points = 200
+        self.gold_card_points = self.gold_card_points - (amount)
+        if amount > self.gold_card_points:
+            print("Not enough points")
+        else:
+            print(self.gold_card_points)
+        return self
+
+
+
+allen = user("Allen", "Wyatt", "wyattallen@dojo.com", "27")
+brandon = user("Brandon", "James", "jamesb@dojo.com", "28")
+
+
+allen.spend_points(50).display_info()
+brandon.enroll().spend_points(80).display_info()
+
